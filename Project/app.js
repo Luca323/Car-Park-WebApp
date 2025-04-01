d3b0d80622e5f2b7a94790d9e0ca26eec48de944
Luca323 <uht23etu@uea.ac.uk>
2025-04-01 17:46:16 +0100

    database connection

--- Project/app.js
+++ Project/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql')
 const app = express();
 const port = 8080;
 
@@ -8,4 +9,19 @@ app.get('/',(req,res) => {
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
+});
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password: "",
+    database: "parkease"
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as ID', connection.threadId);
 });
