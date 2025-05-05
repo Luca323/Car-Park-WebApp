54d2a926f5053789ae5cc0349b2cec05b264917a
rhyskennedy <152311923+rhyskennedy@users.noreply.github.com>
2025-05-05 13:42:16 +0100

    Update app.js

--- Project/app.js
+++ Project/app.js
@@ -65,7 +65,6 @@ app.post('/login', (req, res) => {
 });
 
 
-const bcrypt = require('bcrypt');
 const saltRounds = 10; // You can adjust this based on desired security/performance
 
 // Registration endpoint
